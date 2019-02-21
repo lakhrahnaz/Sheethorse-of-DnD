01a67b8a4f0b10a12ffdd5c683d162e9e3b3ec83
DESKTOP-M10U063\Justin Eng <lakhrahnaz@gmail.com>
2019-02-21 19:59:29 +0800

    First commit version 3

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+import javax.security.auth.login.LoginException;
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException, IOException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String input = new String(Files.readAllBytes(Paths.get("token.txt")));
+        String token = input;
+        //String token = "NTQ4MDI1OTcwOTE3OTAwMjg5.D0_eZg.4Xi5n3WBpgDSujUMWYZsSIAyrbY";
+        builder.setToken(token);
+        builder.addEventListener(new Main());
+        builder.buildAsync();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("Message received from " +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay()
+        );
+        if(event.getAuthor().isBot()) {
+            return;
+        }
+        if(event.getMessage().getContentRaw().equals("!ass")) {
+            event.getChannel().sendMessage("ASS").queue();
+        }
+        else if (event.getMessage().getContentRaw().contains("ass")) {
+            event.getChannel().sendMessage("asses").queue();
+        }
+    }
+
+
+}
